1a0804044e6dfe43e2c6c1fb86617522b2853c7b
aiWen_Company <yuanyuan_pyy@163.com>
2017-03-16 11:18:10 +0800

    creat new project for iOS

---
+++ TestCocoaPodsDemo/TestClass/TestPod.h
@@ -0,0 +1,15 @@
+//
+//  TestPod.h
+//  TestCocoaPodsDemo
+//
+//  Created by Snake on 17/3/16.
+//  Copyright © 2017年 IAsk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestPod : NSObject
+
+- (void)showPod;
+
+@end
---
+++ TestCocoaPodsDemo/TestClass/TestPod.m
@@ -0,0 +1,15 @@
+//
+//  TestPod.m
+//  TestCocoaPodsDemo
+//
+//  Created by Snake on 17/3/16.
+//  Copyright © 2017年 IAsk. All rights reserved.
+//
+
+#import "TestPod.h"
+
+@implementation TestPod
+- (void)showPod {
+    NSLog(@"you can see");
+}
+@end
